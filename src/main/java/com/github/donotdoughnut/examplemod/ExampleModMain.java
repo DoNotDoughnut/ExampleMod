4f9a24e062264fd6e9f7375c56be9f4ca32e1a7a
Rhys Holloway <rhysholloway@pacbell.net>
2020-01-27 19:24:01 -0800

    changed gradle files
    no curios :(
    
    Signed-off-by: Rhys Holloway <rhysholloway@pacbell.net>

--- src/main/java/com/github/donotdoughnut/examplemod/ExampleModMain.java
+++ src/main/java/com/github/donotdoughnut/examplemod/ExampleModMain.java
@@ -9,11 +9,15 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 @Mod(ExampleModMain.MOD_ID)
 public class ExampleModMain {
@@ -29,15 +33,26 @@ public class ExampleModMain {
 	
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientInit);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
 		
         MinecraftForge.EVENT_BUS.register(this);
 	}
 	
 	private void init(final FMLCommonSetupEvent event) {    
 		
-        LOGGER.info(NAME+" initialized.");
+		
+		
+        LOGGER.info(NAME+": Initialized.");
     }
 
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+
+    	//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("accessory").setSize(2).setEnabled(true));
+    	
+    	LOGGER.info(NAME+": Curios initialized");
+    }
+	
     private void clientInit(final FMLClientSetupEvent event) {
 
     	LOGGER.info(NAME+": Client initialized");
