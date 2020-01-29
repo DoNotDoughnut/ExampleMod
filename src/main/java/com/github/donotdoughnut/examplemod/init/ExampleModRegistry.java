95fcb86c2362a6cea1632e001d97c0199b496242
Rhys Holloway <rhysholloway@pacbell.net>
2020-01-28 21:30:47 -0800

    First Commit INDEV-3
    added name for obsidian skull,
    more info in comments in mod registry
    
    Signed-off-by: Rhys Holloway <rhysholloway@pacbell.net>

--- src/main/java/com/github/donotdoughnut/examplemod/init/ExampleModRegistry.java
+++ src/main/java/com/github/donotdoughnut/examplemod/init/ExampleModRegistry.java
@@ -37,53 +37,76 @@ public class ExampleModRegistry {
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class ExampleModRegister {
-		
+
 		@SubscribeEvent
 		public static void registerItems(RegistryEvent.Register<Item> event) {
 			event.getRegistry().registerAll(
 
-					hallowed_ingot = new ITEM("hallowed_ingot"),																	// Hallowed Bar
-					
-					hallowed_sword = new SWORD(hallowed, 5, -2.4f), 																// Excalibur
-					hallowed_pickaxe_axe = new MULTITOOL(hallowed, 3, -2.8f, "pickaxe_axe", ToolType.PICKAXE, ToolType.AXE),		// Pickaxe Axe
-					//hallowed_bow = new CROSSBOW(ITEMTYPE.hallowed, 0, 0, "hallowed_bow"), 										// Hallowed Repeater (Removed)
-					
-					hallowed_ingot_block_item = new BLOCKITEM(hallowed_ingot_block),												// Hallowed Bar Stack
-					
-					hallowed_ranged_helmet = new HallowedArmor(hallowed_ranged, EquipmentSlotType.HEAD),							// Hallowed Helmet
-					hallowed_melee_helmet = new HallowedArmor(hallowed_melee, EquipmentSlotType.HEAD),   							// Hallowed Mask
-					hallowed_magic_helmet = new HallowedArmor(hallowed_magic, EquipmentSlotType.HEAD), 								// Hallowed Headgear
-					
-					hallowed_chestplate = new ARMOR(hallowed_basic, EquipmentSlotType.CHEST),										// Hallowed Plate Mail
-					hallowed_leggings = new ARMOR(hallowed_basic, EquipmentSlotType.LEGS),   										// Hallowed Leggings
-					hallowed_boots = new ARMOR(hallowed_basic, EquipmentSlotType.FEET),												// Hallowed Boots
-					
-					accessory_aglet = new AccessoryAglet(),																			// Aglet
-					accessory_shiny_red_balloon = new AccessoryShinyRedBalloon(),													// Shiny Red Balloon
+					/* Hallowed Bar (Item Name) */
+					hallowed_ingot = new ITEM("hallowed_ingot"),
+
+					/* Excalibur (Item Type, Attack Damage, Attack Speed) */
+					hallowed_sword = new SWORD(hallowed, 5, -2.4f),
+
+					/* Pickaxe Axe (Item Type, Attack Damage, Attack Speed, Name, Types) */
+					hallowed_pickaxe_axe = new MULTITOOL(hallowed, 3, -2.8f, "pickaxe_axe", ToolType.PICKAXE,
+							ToolType.AXE),
+
+					/* Hallowed Repeater (Removed) */
+					// hallowed_bow = new CROSSBOW(ITEMTYPE.hallowed, 0, 0, "hallowed_bow"),
+
+					/* Hallowed Bar Stack (Base block) */
+					hallowed_ingot_block_item = new BLOCKITEM(hallowed_ingot_block),
+
+					/* Hallowed Helmet (Armor type, Armor slot) */
+					hallowed_ranged_helmet = new HallowedArmor(hallowed_ranged, EquipmentSlotType.HEAD),
+
+					/* Hallowed Mask (Armor type, Armor slot) */
+					hallowed_melee_helmet = new HallowedArmor(hallowed_melee, EquipmentSlotType.HEAD),
+
+					/* Hallowed Headgear (Armor type, Armor slot) */
+					hallowed_magic_helmet = new HallowedArmor(hallowed_magic, EquipmentSlotType.HEAD),
+
+					/* Hallowed Plate Mail (Armor type, Armor slot) */
+					hallowed_chestplate = new ARMOR(hallowed_basic, EquipmentSlotType.CHEST),
+
+					/* Hallowed Leggings (Armor type, Armor slot) */
+					hallowed_leggings = new ARMOR(hallowed_basic, EquipmentSlotType.LEGS),
+
+					/* Hallowed Boots (Armor type, Armor slot) */
+					hallowed_boots = new ARMOR(hallowed_basic, EquipmentSlotType.FEET),
+
+					/* Aglet () */
+					accessory_aglet = new AccessoryAglet(),
+
+					/* Shiny Red Balloon () */
+					accessory_shiny_red_balloon = new AccessoryShinyRedBalloon(),
+
+					/* Obsidian Skull (Max burn time) */
 					accessory_obsidian_skull = new AccessoryObsidianSkull(2)
-					
-					);
+
+			);
 
 			LOGGER.info(NAME + ": Items registered.");
 		}
-		
+
 		@SubscribeEvent
 		public static void registerBlocks(RegistryEvent.Register<Block> event) {
-			
+
 			// DONT FORGET TO CREATE BLOCKITEM WHEN REGISTERING BLOCKS
-			
+
 			event.getRegistry().registerAll(
 
 					hallowed_ingot_block = new BLOCK(IRON, 3.0f, 3.0f, 3, METAL, "hallowed_ingot_block")
-					
+
 			);
-			
-			LOGGER.info(NAME+": Blocks registered");
-			
+
+			LOGGER.info(NAME + ": Blocks registered");
+
 		}
-		
-	}	
-	
+
+	}
+
 	public static class ITEM extends Item {
 
 		public ITEM(String regName) {
@@ -92,10 +115,11 @@ public class ExampleModRegistry {
 		}
 
 	}
-	
+
 	public static class BLOCK extends Block {
 
-		public BLOCK(Material mat, float hardness, float resisitance, int lightValue, SoundType walkSound, String registryName) {
+		public BLOCK(Material mat, float hardness, float resisitance, int lightValue, SoundType walkSound,
+				String registryName) {
 			super(Block.Properties.create(mat).hardnessAndResistance(3.0f, 3.0f).lightValue(1).sound(walkSound));
 			this.setRegistryName(MOD_ID, registryName);
 		}
@@ -110,15 +134,17 @@ public class ExampleModRegistry {
 		}
 
 	}
-	
+
 	public static class MULTITOOL extends ExampleModMultitool {
 
-		public MULTITOOL(ITEMTYPE tier, float attackDamageIn, float attackSpeedIn, String registryName, ToolType type1, ToolType type2) {
+		public MULTITOOL(ITEMTYPE tier, float attackDamageIn, float attackSpeedIn, String registryName, ToolType type1,
+				ToolType type2) {
 			super(tier, attackDamageIn, attackSpeedIn, new Item.Properties().group(GROUP_TOOLS), type1, type2);
 			this.setRegistryName(MOD_ID, tier.getType() + "_multitool_" + registryName);
 		}
-		
-		public MULTITOOL(ITEMTYPE tier, float attackDamageIn, float attackSpeedIn, String registryName, ToolType type1, ToolType type2, ToolType type3) {
+
+		public MULTITOOL(ITEMTYPE tier, float attackDamageIn, float attackSpeedIn, String registryName, ToolType type1,
+				ToolType type2, ToolType type3) {
 			super(tier, attackDamageIn, attackSpeedIn, new Item.Properties().group(GROUP_TOOLS), type1, type2, type3);
 			this.setRegistryName(MOD_ID, tier.getType() + "_multitool_" + registryName);
 		}
@@ -150,19 +176,19 @@ public class ExampleModRegistry {
 
 			String armorSlot = getSlotName(slot.getSlotIndex()), variant = material.getVariant();
 
-			if(variant == null || variant == "")
+			if (variant == null || variant == "")
 				this.setRegistryName(MOD_ID, material.getType() + "_" + armorSlot);
 			else
 				this.setRegistryName(MOD_ID, material.getType() + "_" + variant + "_" + armorSlot);
 		}
 
 		/*
-		public ARMOR(ARMORTYPE material, EquipmentSlotType slot, String registryName) {
-			super(material, slot, new Item.Properties().group(GROUP_ARMOR));
-			this.setRegistryName(MOD_ID, material.getType() + "_" + material.getBasicName() + "_armor_" + registryName);
-		}
-		*/
-		
+		 * public ARMOR(ARMORTYPE material, EquipmentSlotType slot, String registryName)
+		 * { super(material, slot, new Item.Properties().group(GROUP_ARMOR));
+		 * this.setRegistryName(MOD_ID, material.getType() + "_" +
+		 * material.getBasicName() + "_armor_" + registryName); }
+		 */
+
 		private String getSlotName(int i) {
 			if (i == 1)
 				return "boots";
@@ -172,7 +198,7 @@ public class ExampleModRegistry {
 				return "chestplate";
 			else if (i == 4)
 				return "helmet";
-			else 
+			else
 				return "none";
 		}
 
@@ -182,19 +208,19 @@ public class ExampleModRegistry {
 
 		public ACCESSORY(String registryName) {
 			super(new Item.Properties().group(GROUP_ACCESSORIES));
-			this.setRegistryName(MOD_ID, "accessory_"+registryName);
+			this.setRegistryName(MOD_ID, "accessory_" + registryName);
 		}
 
 		@Override
 		public ICapabilityProvider initCapabilities(ItemStack stack, CompoundNBT unused) {
 			return CapCurioItem.createProvider(this);
 		}
-		
+
 		@Override
 		public boolean canRightClickEquip() {
-		    return true;
-		  }
+			return true;
+		}
 
 	}
-	
+
 }
